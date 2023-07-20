d6cdc990d3ad0b6b48ae73973b8bdc110ad1432c
David Jaffe <99.david.b.jaffe@gmail.com>
2023-07-20 12:57:58 -0700

    add a data type

--- binary_vec_io/src/lib.rs
+++ binary_vec_io/src/lib.rs
@@ -23,6 +23,7 @@ impl BinaryInputOutputSafe for u64 {}
 impl BinaryInputOutputSafe for f32 {}
 impl BinaryInputOutputSafe for f64 {}
 impl BinaryInputOutputSafe for ([u8; 5], [u8; 3]) {}
+impl BinaryInputOutputSafe for ([u8; 20], u32, u32) {}
 // i128, u128?
 
 use std::io::Error;
