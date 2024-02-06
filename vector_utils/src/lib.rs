a2941c400a7b19476f81837cbd70207a2dfa36cb
David Jaffe <99.david.b.jaffe@gmail.com>
2024-02-06 07:13:38 -0800

    add reverse_sort_sync_by3

--- vector_utils/src/lib.rs
+++ vector_utils/src/lib.rs
@@ -533,6 +533,17 @@ pub fn reverse_sort_sync3<T: Ord + Clone, S1: Ord + Clone, S2: Ord + Clone>(
     s2.reverse();
 }
 
+pub fn reverse_sort_sync_by3<T: Ord + Clone, S1: Ord + Clone, S2: Ord + Clone>(
+    t: &mut Vec<T>,
+    s1: &mut Vec<S1>,
+    s2: &mut Vec<S2>,
+) {
+    let permutation = permutation::sort_by(&t[..], |a, b| b.partial_cmp(a).unwrap());
+    *t = permutation.apply_slice(&t[..]);
+    *s1 = permutation.apply_slice(&s1[..]);
+    *s2 = permutation.apply_slice(&s2[..]);
+}
+
 pub fn sort_sync4<T: Ord + Clone, S1: Ord + Clone, S2: Ord + Clone, S3: Ord + Clone>(
     t: &mut Vec<T>,
     s1: &mut Vec<S1>,
