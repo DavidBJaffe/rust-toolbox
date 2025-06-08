087e9f5e4ab6db639fc25984a9383041e9198571
David Jaffe <99.david.b.jaffe@gmail.com>
2025-06-08 12:11:05 -0700

    try fix

--- tables/src/lib.rs
+++ tables/src/lib.rs
@@ -718,8 +718,12 @@ pub fn print_tabular_vbox(
                     log.push(dash_bold);
                 }
             } else if rrr[rrr.len() - 1][i + 1] != "\\ext" {
-                if vert_bold[i] && opt.bold_outer {
-                    log.push(uptee_bold_bold);
+                if opt.bold_outer {
+                    if vert_bold[i] {
+                        log.push(uptee_bold_bold);
+                    } else {
+                        log.push(uptee_bold2);
+                    }
                 } else {
                     log.push(uptee);
                 }
